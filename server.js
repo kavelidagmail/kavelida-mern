513c41245d0ccf4095fd7f1e4df9f48c82fcb7a0
kavelidagmail <kaushik.velidandla3@gmail.com>
2023-11-15 12:47:05 -0800

    deleting build folder from source control and removing express static

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post("/create", (req, res) => {
 });
 
 // production script
-app.use(express.static("./client/build"));
+//app.use(express.static("./client/build"));
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
